628b2e7e87c4529a0a9ee039db905f41e89474f4
xiazdong <xiazdong@126.com>
2017-04-16 19:27:10 +0800

    第四章：线性插值实现渐变色

--- chapter4/src/main/java/xiazdong/me/chapter4/AirHockyRender.java
+++ chapter4/src/main/java/xiazdong/me/chapter4/AirHockyRender.java
@@ -16,16 +16,11 @@ import xiazdong.me.util.TextResourceReader;
 
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.GL_FLOAT;
-import static android.opengl.GLES20.GL_LINES;
-import static android.opengl.GLES20.GL_POINTS;
-import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.*;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
-import static android.opengl.GLES20.glDrawArrays;
 import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
-import static android.opengl.GLES20.glGetUniformLocation;
-import static android.opengl.GLES20.glUniform4f;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 
@@ -37,40 +32,33 @@ import static android.opengl.GLES20.glVertexAttribPointer;
 public class AirHockyRender implements GLSurfaceView.Renderer {
 
     private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int BYTES_PER_FLOAT = 4;
-    private static final String U_COLOR = "u_Color";
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
     private static final String A_POSITION = "a_Position";
+    private static final String A_COLOR = "a_Color";
     private final FloatBuffer vertexData;
     private Context context;
     private int program;
-    private int uColorLocation;
+    private int aColorLocation;
     private int aPositionLocation;
 
     public AirHockyRender(Context context) {
         //顶点属性数组
         float[] tableVertices = {
-                //桌子边框
-                -0.55f, 0.53f,
-                0.55f, 0.53f,
-                -0.55f, -0.53f,
-
-                0.55f, 0.53f,
-                -0.55f, -0.53f,
-                0.55f, -0.53f,
                 //桌子
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                -0.5f, 0.5f,
-
-                0.5f, -0.5f,
-                -0.5f, 0.5f,
-                0.5f, 0.5f,
+                0, 0, 1f, 1f, 1f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
                 //中间线
-                -0.5f, 0f,
-                0.5f, 0f,
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
                 //木椎
-                0f, 0.25f,
-                0f, -0.25f
+                0f, 0.25f, 0f, 0f, 1f,
+                0f, -0.25f, 1f, 0f, 0f
         };
         vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT)
                                 .order(ByteOrder.nativeOrder()).asFloatBuffer();
@@ -90,7 +78,7 @@ public class AirHockyRender implements GLSurfaceView.Renderer {
         }
         glUseProgram(program);
 
-        uColorLocation = glGetUniformLocation(program, U_COLOR);
+        aColorLocation = glGetAttribLocation(program, A_COLOR);
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
         vertexData.position(0);
         /**
@@ -98,8 +86,11 @@ public class AirHockyRender implements GLSurfaceView.Renderer {
          * 2. 一个属性有多少个数据，这里是(x,y)，因此是2
          * 3. 数据类型
          */
-        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0, vertexData);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
         glEnableVertexAttribArray(aPositionLocation);
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aColorLocation);
 
     }
 
@@ -111,19 +102,12 @@ public class AirHockyRender implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         glClear(GL_COLOR_BUFFER_BIT);
-        //桌子边框
-        glUniform4f(uColorLocation, 1f, 0f, 1f, 1f);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
         //桌子
-        glUniform4f(uColorLocation, 1f, 1f, 1f, 1f); //设置shader中u_Color的值
-        glDrawArrays(GL_TRIANGLES, 6, 6); //第三个参数：读入6个顶点
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6); //第三个参数：读入6个顶点
         //中间线
-        glUniform4f(uColorLocation, 1f, 0f, 0f, 1f);
-        glDrawArrays(GL_LINES, 12, 2);
+        glDrawArrays(GL_LINES, 6, 2);
         //木椎
-        glUniform4f(uColorLocation, 0f, 0f, 1f, 1f);
-        glDrawArrays(GL_POINTS, 14, 1);
-        glUniform4f(uColorLocation, 1f, 0f, 0f, 1f);
-        glDrawArrays(GL_POINTS, 15, 1);
+        glDrawArrays(GL_POINTS, 8, 1);
+        glDrawArrays(GL_POINTS, 9, 1);
     }
 }
